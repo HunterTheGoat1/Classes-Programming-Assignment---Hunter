6a84a81226f9e191b8caca65a5a6f2c639a5a881
Hunter <WilsHunt604@gotvdsb.ca>
2022-10-06 09:24:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Classes_Programming_Assignment___Hunter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student person = new Student("Robert", "Ross");
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person.LastName);
+            Console.WriteLine(person.StudentNumber);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Programming_Assignment___Hunter
+{
+    internal class Student
+    {
+        private static Random generator = new Random();
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private int _studentNumber;
+        public Student(string firstName, string lastName)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._studentNumber = 555000 + generator.Next(0, 1000);
+        }
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                this._firstName=value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                this._lastName = value;
+            }
+        }
+        public int StudentNumber
+        {
+            get 
+            { 
+                return _studentNumber; 
+            }
+        }
+    }
+}
